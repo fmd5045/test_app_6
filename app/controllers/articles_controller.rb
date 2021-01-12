adbeefb8a8d7875be3791e8d103b4f9daace03c1
fmd5045 <freddy.duluc@gmail.com>
2021-01-12 11:30:05 -0700

    Added the articles listing template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
-def show
+    def show
 
-    #@makes the variable into an instance variable, this make it availble on the show view
-    #params allows you to pass parameter, its a hash data structure
-    @article = Article.find(params[:id])
+        #@makes the variable into an instance variable, this make it availble on the show view
+        #params allows you to pass parameter, its a hash data structure
+        @article = Article.find(params[:id])
 
-
-end
+    end
+    def
+    index
+        @articles = Article.all
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   #exclude all other routes and only show the "show" action
-  resources :articles, only: [:show]
+  resources :articles , only: [:show, :index]
 end
