20f006bd7bcd438c9f3ec7f8fdde9dbdbf0eed61
fmd5045 <freddy.duluc@gmail.com>
2021-02-16 15:35:29 -0700

    Add users table and user model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 6, maximum:100 }
     #make sure that the description is present in order to save the article
     validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+    belongs_to :user
 end
